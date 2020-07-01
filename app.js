cc2393080e3c6f35a7c585e518a5666f510c1e0e
Marie Garnaud <garnaudm.e@gmail.com>
2020-07-01 10:25:03 +0200

    mongoose

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var profilRouter = require("./routes/profil");
 const productsRouter = require("./routes/products");
+const mongoose = require("mongoose");
 
 var app = express();
 
@@ -14,6 +15,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// Database connection
+
+mongoose.connect("mongodb://localhost:27017/api-back-martine", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("Database Connected");
+});
+
+mongoose.connection.on("error", () => {
+  console.log("Error connection database");
+});
 
 app.use("/", indexRouter);
 app.use("/profil", profilRouter);
