123f865c0798d9ce3179e49b189a0d21be42c3ec
Mohamed iliasTamedi <tam.ilias22000@gmail.com>
2020-07-07 15:07:51 +0200

    martine back

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,19 @@ const Product = require("../model/Product");
 const Profil = require("../model/Profil");
 const productController = {
   //enregistrement du product dans la base de donnée
-  product: (req, res) => {
+  productCreate: (req, res) => {
+    console.log(req.file.path);
+
+    const filePath = req.file.path.replace("public", "");
+
     const newProduct = new Product({
+      category: req.body.category,
       travel_name: req.body.travel_name,
       short_description: req.body.short_description,
       long_description: req.body.long_description,
       main_picture: req.body.main_picture,
-      picture: req.body.picture,
+      picture: [{ original: filePath, thumbnail: filePath }],
+
       price: req.body.price,
     });
     newProduct.save((err) => {
--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,15 @@
 var express = require("express");
 const productController = require("../controllers/products");
+const multer = require("../middlewares/multer");
 var router = express.Router();
-const productController = require("../controllers/products");
 
 /* GET users listing. */
+router.post("/new-travels", multer, productController.productCreate);
+router.get("/new-travels", productController.getProduct);
+router.get("/category/:category", productController.categoryProduct);
 router.get("/:product", productController.product);
+
 router.post("/reservation", productController.reservation);
 router.get("/more/:product", productController.more);
 
-router.get("/new-travels", productController.getProduct);
-router.get("/category/:category", productController.categoryProduct);
 module.exports = router;
